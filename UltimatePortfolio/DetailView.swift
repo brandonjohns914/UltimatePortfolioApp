4e5b173b3478be6590913365cf8aaae060c36322
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-01-17 17:08:09 -0800

    initial commit

---
+++ UltimatePortfolio/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  UltimatePortfolio
+//
+//  Created by Brandon Johns on 1/17/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,27 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Brandon Johns on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    @StateObject var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationSplitView {
+                SidebarView()
+            } content: {
+                ContentView()
+            } detail: {
+                DetailView()
+            }
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+        }
+    }
+}
